e610bd8b5250498a6163d9d8c6cb3c6870e2ff5c
Rachel Ramsay <rachel.ramsay@outlook.com>
2017-02-17 15:04:32 -0800

    Improve error handling and fix home page bug

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import os
 import re
+import requests
 
 from flask import Flask, flash, redirect, render_template, request, session
 
@@ -17,6 +18,17 @@ base_url = CloverAPI.base_url
 def oauth_dance():
     print "Performing OAuth..."
 
+    # Ensure that the merchant did not enter the web app URL directly and that
+    # their mId is present.
+    merchant_id = request.args.get('merchant_id', None)
+    pattern = re.compile("clover")
+
+    # There could be no referrer, or the referrer could be not-Clover, or the
+    # request URL could be missing the mId argument.
+    if not request.referrer or not pattern.search(request.referrer) or not merchant_id:
+        flash("Please launch this web app from Clover Home web dashboard.")
+        return render_template("base.html")
+
     # Syntax is .args for GET, .form for POST.
     code = request.args.get('code', None)
     if not code:
@@ -30,32 +42,18 @@ def oauth_dance():
     except requests.exceptions.HTTPError:
         raise Exception("OAuth request failed!")
 
-    try:
-        access_token = resp["access_token"]
-        merchant_id = request.args["merchant_id"]
-    except KeyError:
-        raise Exception("Failed to capture required API wrapper parameters!")
-
     # Store in the session so we can use them later.
-    session["access_token"] = access_token
-    session["merchant_id"] = merchant_id
+    try:
+        session["access_token"] = resp["access_token"]
+        session["merchant_id"] = request.args["merchant_id"]
+    except KeyError:
+        raise Exception("Failed to capture required API wrapper args!")
+
     print "OAuth successful!"
 
 
 @app.route('/', methods=['GET'])
 def home_page():
-
-    # Ensure that the merchant did not enter the web app URL directly and that
-    # their mId is present.
-
-    merchant_id = request.args.get('merchant_id', None)
-    pattern = re.compile("clover")
-
-    # There could be no referrer, or the referrer could be not-Clover, or the
-    # request URL could be missing the mId argument.
-    if not request.referrer or not pattern.search(request.referrer) or not merchant_id:
-        flash("Please launch this web app from Clover Home web dashboard.")
-        return render_template("base.html")
 
     #Get OAuth token if needed.
     if not session.get("access_token"):
@@ -66,7 +64,12 @@ def home_page():
     # Create CloverAPI instance with access token and merchant id.
     c = CloverAPI(session.get("access_token"), session.get("merchant_id"))
     # Make API call.
-    merchant = c.get("/v3/merchants/{mId}")
+
+    try:
+        merchant = c.get("/v3/merchants/{mId}")
+    except requests.exceptions.HTTPError:
+        #TODO: Error handling.
+        raise Exception("API call failed.")
 
     return render_template("home.html",
                            merchant=merchant)
@@ -81,8 +84,12 @@ def order_list():
     '''Display orders including line items.'''
 
     c = CloverAPI(session.get("access_token"), session.get("merchant_id"))
-    resp = c.get("/v3/merchants/{mId}/orders",
-                 expand="lineItems")
+    try:
+        resp = c.get("/v3/merchants/{mId}/orders",
+                     expand="lineItems")
+    except requests.exceptions.HTTPError:
+        #TODO: Error handling.
+        raise Exception("API call failed.")
 
     # The response dict contains `href` and `elements`, the latter being the
     # list of orders.
@@ -110,10 +117,14 @@ def create_order():
     # appear in the Orders app."
 
     c = CloverAPI(session.get("access_token"), session.get("merchant_id"))
-    resp = c.post("/v3/merchants/{mId}/orders",
-                  {"state": "open",
-                   "total": total,
-                   "note": note})
+    try:
+        resp = c.post("/v3/merchants/{mId}/orders",
+                      {"state": "open",
+                       "total": total,
+                       "note": note})
+    except requests.exceptions.HTTPError:
+        #TODO: Error handling.
+        raise Exception("API call failed.")
 
     order_id = resp.id
     message = "Order #{} created.".format(order_id)
@@ -129,19 +140,27 @@ def order_ice_cream():
     c = CloverAPI(session.get("access_token"), session.get("merchant_id"))
 
     # Create order.
-    resp = c.post("/v3/merchants/{mId}/orders",
-                  {"state": "open",
-                   "note": "ice cream"})
+    try:
+        resp = c.post("/v3/merchants/{mId}/orders",
+                      {"state": "open",
+                       "note": "ice cream"})
+    except requests.exceptions.HTTPError:
+        #TODO: Error handling.
+        raise Exception("API call failed.")
 
     # Get ID from response.
     order_id = resp.id
 
     # Add line item to order. Nb. At a minimum this request must include a price
     # or a reference to an inventory item.
-    resp = c.post("/v3/merchants/{mId}/orders/{orderId}/line_items",
-                  {"name": "ice cream",
-                   "price": 100},
-                  orderId=order_id)
+    try:
+        resp = c.post("/v3/merchants/{mId}/orders/{orderId}/line_items",
+                      {"name": "ice cream",
+                       "price": 100},
+                      orderId=order_id)
+    except requests.exceptions.HTTPError:
+        #TODO: Error handling.
+        raise Exception("API call failed.")
 
     message = "You ordered ice cream!"
     flash(message)
@@ -156,15 +175,19 @@ def update_orders(interaction):
     c = CloverAPI(session.get("access_token"), session.get("merchant_id"))
 
     if request.method == "GET":
+        try:
             resp = c.get("/v3/merchants/{mId}/orders")
+        except requests.exceptions.HTTPError:
+            #TODO: Error handling.
+            raise Exception("API call failed.")
 
-            # The response dict contains `href` and `elements`, the latter being the
-            # list of orders.
-            orders = resp["elements"]
+        # The response dict contains `href` and `elements`, the latter being the
+        # list of orders.
+        orders = resp["elements"]
 
-            return render_template("orders.html",
-                                   orders=orders,
-                                   interaction=interaction)
+        return render_template("orders.html",
+                               orders=orders,
+                               interaction=interaction)
 
     # Make POST requests.
     # Syntax is `.getlist` because multiple checkboxes may have been checked.
@@ -173,13 +196,21 @@ def update_orders(interaction):
     for order_id in order_ids:
 
         if interaction == "close":
-            c.post("/v3/merchants/{mId}/orders/{orderId}",
-                   {"state": "closed"},  # Setting this field to null means order can only be GET by id.
-                   orderId=order_id)
+            try:
+                c.post("/v3/merchants/{mId}/orders/{orderId}",
+                       {"state": "closed"},  # Setting this field to null means order can only be GET by id.
+                       orderId=order_id)
+            except requests.exceptions.HTTPError:
+                #TODO: Error handling.
+                raise Exception("API call failed.")
 
         elif interaction == "delete":
-            c.delete("/v3/merchants/{mId}/orders/{orderId}",
-                     orderId=order_id)
+            try:
+                c.delete("/v3/merchants/{mId}/orders/{orderId}",
+                         orderId=order_id)
+            except requests.exceptions.HTTPError:
+                #TODO: Error handling.
+                raise Exception("API call failed.")
 
         message = "Order #{} {}d.".format(order_id, interaction)
         flash(message)
